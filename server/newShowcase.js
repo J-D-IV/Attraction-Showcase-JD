c4643c5ed1126868ae529d18fe657f368780a4e7
JD <jd.havrilla1@gmail.com>
2021-02-07 13:24:18 -0700

    added matching queries to legacy api calls

--- server/newShowcase.js
+++ server/newShowcase.js
@@ -1,16 +1,42 @@
+// const { response } = require('express');
 const express = require('express');
 const client = require('../db/newIndex');
 
 const showcase = express.Router();
 
 showcase.get('/api/showcase', (req, res) => {
-  client.query('SELECT ratio FROM showcase WHERE showcase_id=9334244', (err, response) => {
+  client.query('SELECT * FROM showcase WHERE ratio=1', (err, response) => {
     if (err) {
       console.error(err);
       return;
     }
     console.log(response.rows);
     res.status(200).send(response);
+  });
+});
+
+showcase.get('/api/showcase/:id', (req, res) => {
+  const { id } = req.params;
+  client.query(`SELECT * FROM showcase WHERE id=${id}`, (err, response) => {
+    if (err) {
+      console.log(err);
+      // return;
+    } else {
+      res.status(200).send(response);
+    }
+  });
+});
+
+showcase.post('/api/showcase/:attractionId', (req, res) => {
+  const { form } = req.body;
+  const { attractionId } = req.params;
+  const obj = { ...form, attractionId };
+  client.query(`INSERT INTO improveForm (attraction_id, description, isOpen, suggestedDuration, address) VALUES (${obj})`, (err, response) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(response);
+    }
   });
 });
 
